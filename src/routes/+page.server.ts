9f7c57c4fabf71d8be4e1ee1630dab037f27df55
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2024-03-20 23:18:09 +0700

    use blunder code (again), this time to fix VSCode Svelte extension's broken intellisense

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -11,5 +11,16 @@ export const load: PageServerLoad = async () => {
 		.sort("xata.createdAt", "desc")
 		.getFirst();
 
-	return data;
+	// Blunder code 2: Electric boogaloo
+	return {
+		temperature: data?.temperature!,
+		humidity: data?.humidity!,
+		pressure: data?.pressure!,
+		windSpeedAvg: data?.windSpeedAvg!,
+		windSpeedMax: data?.windSpeedMax!,
+		windDirection: data?.windDirection!,
+		rainfall_D: data?.rainfall_D!,
+		rainfall_H: data?.rainfall_H!,
+		xata: data?.xata!
+	};
 }
