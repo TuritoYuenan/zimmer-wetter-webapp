fb55e8016220bc42cdd23e039568e66fdd61cc17
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2024-03-21 08:00:26 +0700

    temporarily track xata lib file again

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,5 @@
 import type { PageServerLoad } from './$types';
-import { XataClient } from '../xata';
+import { XataClient } from '$lib/xata';
 
 const xata = new XataClient({
 	apiKey: import.meta.env.VITE_XATA_API_KEY,
